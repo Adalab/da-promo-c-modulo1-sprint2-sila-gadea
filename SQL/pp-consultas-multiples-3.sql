3e178fa1906aef0d91e472703f5d62a9d3c5fbb8
gadeatric <cgautric@gmail.com>
2022-11-27 13:19:36 +0100

    Consultas múltiples 3

--- SQL/pp-consultas-multiples-3.sql
+++ SQL/pp-consultas-multiples-3.sql
@@ -9,12 +9,11 @@ Mostrad la ciudad a la que pertenecen, el nombre de la empresa y el nombre del c
 Para ello añade el valor que le quieras dar al campo y utilizada como alias Relationship.*/
 
 
-SELECT 'Customers' AS Relationship, company_name AS NombreCompañia, city AS Ciudad, contact_name AS NombreContacto
+SELECT 'Customers' AS RELATIONSHIP, company_name AS NOMBRE_COMPAÑIA, city AS CIUDAD, contact_name AS NOMBRE_CONTACTO
 FROM customers
 UNION
-SELECT 'Suppliers' AS Relationship, company_name AS NombreCompañia, city AS Ciudad, contact_name AS NombreContacto
+SELECT 'Suppliers' AS Relationship, company_name AS NOMBRE_COMPAÑIA, city AS CIUDAD, contact_name AS NOMBRE_CONTACTO
 FROM suppliers;
-
 
 /*EJERCICIO 2.
 Extraer todos los pedidos gestinados por "Nancy Davolio"
@@ -23,40 +22,33 @@ una de nuestras empleadas. Nos pide todos los detalles tramitados por ella.
 Los resultados de la query deben parecerse a la siguiente tabla:*/
 #tablas:
 
-SELECT employees.first_name, employees.last_name, orders.order_date
-FROM employees, orders
-WHERE employees.employee_id = orders.employee_id AND first_name = 'Nancy' AND last_name = 'Davolio';
-
-SELECT * 
+SELECT *
 FROM orders
-WHERE employee_id in (
-	select employee_id, first_name, last_name
+WHERE employee_id IN (
+	SELECT employee_id
 	FROM employees
-	WHERE first_name = 'Nancy' AND last_name = 'Davolio');
+	WHERE employees.employee_id = orders.employee_id AND first_name = 'Nancy' AND last_name = 'Davolio');
 
 /*EJERCICIO 3.
 Extraed todas las empresas que no han comprado en el año 1997
 En este caso, nuestro jefe quiere saber cuántas empresas no han comprado en el año 1997. */
 
-SELECT company_name, country
-FROM customers
-WHERE customer_id in (
-	SELECT customer_id
-	from orders
-	where year(order_date) != 1997);
-
+SELECT company_name AS NOM_EMPRESA, country AS PAIS, YEAR(order_date)
+FROM customers, orders
+WHERE orders.customer_id IN (
+	SELECT orders.customer_id
+	FROM orders
+	WHERE YEAR(order_date) <> 1997)
+GROUP BY company_name;
+    
 /*Extraed toda la información de los pedidos donde tengamos "Konbu"
 Estamos muy interesados en saber como ha sido la evolución de la venta de Konbu a lo largo del tiempo.
 Nuestro jefe nos pide que nos muestre todos los pedidos que contengan "Konbu"*/
 
-SELECT * 
-FROM products
-WHERE products.product_name = 'Konbu';
---
 SELECT *
-FROM orders 
-WHERE order_id IN (
-	SELECT *
-	FROM products);
-
-/*esto es el stock que tenemos*/
+FROM orders INNER JOIN products
+WHERE products.product_id = (
+		SELECT products.product_id AS NUMERO_PRODUCTO
+		FROM products
+		WHERE products.product_name = 'Konbu')
+ORDER BY order_id ASC;
